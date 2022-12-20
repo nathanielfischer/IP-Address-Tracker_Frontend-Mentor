9dcf565a01d198cd02d00975d65a8df02180b7f8
Nathaniel Fischer <nathaniel.f@web.de>
2022-12-20 14:12:37 +0100

    first commit

---
+++ README-template.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./docs/screenshot.png)
+
+### Links
+
+- Solution URL: [URL](https://nathanielfischer.github.io/IP-Address-Tracker_Frontend-Mentor)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS
+- Bootstrap
+- JavaScript
+
+### What I learned
+
+I got a better understanding of using APIs from the client side and how Promises in JavaScript work. 
+
+Furthermore I learned how to implement a functional map on a website.
+
+### Useful resources
+
+- [Leafletjs](https://leafletjs.com/examples/quick-start/) - Open-source JavaScript library
+for mobile-friendly interactive maps.
+- [IP Geolocation API](https://ip-api.com/docs/api:json) - Free API to get an IP's Geolocation informatin.
+
+## Author
+
+- Github - [Nathaniel Fischer](https://github.com/nathanielfischer)
---
+++ index.js
@@ -0,0 +1,127 @@
+var map;
+var myIcon = L.icon({
+  iconUrl: "./images/icon-location.svg",
+  iconSize: [37, 45],
+  iconAnchor: [18, 45],
+});
+
+
+checkIpInput();
+
+
+
+// ---------------------- "EVENT LISTENER" ----------------------
+
+$("#button-addon2").click(function () {
+  checkIpInput();
+});
+
+$(".toggle").click(function () {
+  toggleMode();
+});
+
+
+$('.form-control').keypress(function (e) {
+  if (e.which == 13) {
+    checkIpInput();
+  }
+});
+
+
+
+// ---------------------- MAP API ----------------------
+
+/**
+ * Initializes the map with few of a specified location and sets the marker
+ * @param  {number} lat latitude of the IP location
+ * @param  {number} lon longitude of the IP location
+ */
+function initializeMap(lat, lon) {
+  map = L.map('map').setView([lat, lon], 11);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  let marker = L.marker([lat, lon], { icon: myIcon }).addTo(map);
+}
+
+
+/**
+ * Updates the map to a new location and sets the marker
+ * @param  {number} lat latitude of the IP location
+ * @param  {number} lon longitude of the IP location
+ */
+function updateMap(lat, lon) {
+  map.setView([lat, lon], 11);
+  let marker = L.marker([lat, lon], { icon: myIcon }).addTo(map);
+}
+
+
+
+
+
+// ---------------------- IP Address ----------------------
+
+/**
+ * Gets data from Input field
+ * If no Input: it will use clients IP address
+ * TODO: handle errors
+ */
+function checkIpInput() {
+  let ip = $(".form-control")[0].value;
+  let url = "http://ip-api.com/json/";
+
+  if (ip !== "") {
+    url += ip;
+  }
+
+  getMyIp("GET", url)
+    .then(function (e) {
+      let data = JSON.parse(e.target.response);
+      ip = data.query;
+
+      //checks if the map is already initialized. 
+      if (map === undefined) {
+        initializeMap(data.lat, data.lon);
+      } else {
+        updateMap(data.lat, data.lon);
+      }
+
+      const location = data.city + ", " + data.country;
+      setStats(data.query, location, data.timezone, data.isp);
+    }, function (e) {
+      // handle errors
+    });
+}
+
+
+// API IP Address
+function getMyIp(method, url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = resolve;
+    xhr.onerror = reject;
+    xhr.send();
+  });
+}
+
+
+/**
+ * Updates the stats section in the DOM with the new data
+ * @param  {String} ip
+ * @param  {String} location
+ * @param  {String} timezone
+ * @param  {String} isp
+ */
+function setStats(ip, location, timezone, isp){
+  $(".stats h2").eq(0).text(ip);
+  $(".stats h2").eq(1).text(location);
+  $(".stats h2").eq(2).text(timezone);
+  $(".stats h2").eq(3).text(isp);
+}
+
+
+
